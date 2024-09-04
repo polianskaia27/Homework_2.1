cca16144bd23193f04e5bb341c2e9ee4c9bc5367
sasha_polianskaia <polianskaia27@gmail.com>
2024-09-04 17:27:39 -0300

    Homework 2.1.5

---
+++ task_formula.js
@@ -0,0 +1,16 @@
+let value1;
+let value2;
+let formula;
+
+value1 = 5000;
+value2 = Math.floor(Math.random() * 2500000);
+formula = value1 + value2 / 5 + 17;
+
+console.log(value2);
+console.log(formula);
+
+/* 
+1. деление value2 на 5
+2. сложение value1 и полученного в результате деления значения
+3. к полученному значению в результате п.2 прибавляется 17
+*/
